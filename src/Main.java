734435c6bc5c07b9b6756ae590c5fae3fce5f2f6
GuillaumeSavin <guisav1996@hotmail.fr>
2020-02-28 09:01:14 +0100

    arrange

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
+        DatagramSocket s;
+        DatagramPacket p;
+
         s = new DatagramSocket(port);
         p = new DatagramPacket();
 
